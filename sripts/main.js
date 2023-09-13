055e06b1766263fed9705458491236ddd943a18d
Oelson Rasolofo <isaacrasolofo@gmail.com>
2023-09-13 16:33:22 +0200

    contact send mail work

--- sripts/main.js
+++ sripts/main.js
@@ -2,10 +2,12 @@ import { headerScrollTo } from './header.js';
 import { calculAge } from './about.js';
 import { afficherElement } from './project.js';
 import { dataProject } from './data/data-project.js';
+import { SendEmail } from './contact.js';
 
 const scrollProfil = document.querySelector('.profil-container');
 const scrollAbout = document.querySelector('.about-container');
 const scrollProject = document.querySelector('.js-section-second-title');
+const scrollContact = document.querySelector('.js-contact-title');
 
 const headerName = document.querySelector('.js-header-profil');
 const headerAbout = document.querySelector('.js-header-about');
@@ -15,9 +17,10 @@ const headerContact = document.querySelector('.js-header-contact');
 headerScrollTo(headerName, scrollProfil);
 headerScrollTo(headerAbout, scrollAbout);
 headerScrollTo(headerProject, scrollProject);
-headerScrollTo(headerContact, scrollProfil);
+headerScrollTo(headerContact, scrollContact);
 
-document.querySelector('.about-age').innerHTML = calculAge();
+
+document.querySelector('.about-age').innerText = calculAge()
 
 
 function visibleProjectElement() {
@@ -65,3 +68,45 @@ dataProject.forEach((project) => {
 })
 
 visibleProjectElement();
+
+let formErreur = document.querySelector('.form-erreur');
+
+let fomrName = document.querySelector('#form-name')
+let formMail = document.querySelector('#form-mail')
+let formObject = document.querySelector('#form-subject')
+let formMessage = document.querySelector('#form-message')
+
+document.querySelector('.js-submit-form').addEventListener('click', (e) => {
+    e.preventDefault();
+
+    const emailRegex = /\S+@\S+\.\S+/;
+    if(emailRegex.test(formMail.value) === false) {
+        formErreur.innerText = 'Veuillez entrer une adresse mail valide'
+        return
+    }
+
+    let formInputs = {
+        name: fomrName.value,
+        mail: formMail.value,
+        object: formObject.value,
+        message: formMessage.value
+    }
+
+    for (let [key, value] of Object.entries(formInputs)) {
+        if (!value) {
+            formErreur.innerText = 'Veuillez remplir le formulaire correctement';
+            return
+        }
+
+        formErreur.innerText = '';
+    }
+
+    //SendEmail(formInputs);
+
+    alert('Votre message a bien été envoyer! Merci.')
+
+    fomrName.value = "";
+    formMail.value = "";
+    formObject.value = "";
+    formMessage.value = "";
+})
