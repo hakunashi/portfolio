b8d5234a696bc4c4fc6805d68ef396e992f1aa62
Oelson Rasolofo <isaacrasolofo@gmail.com>
2023-09-13 22:53:38 +0200

    accurate data project and update youtube project

--- sripts/data/data-project.js
+++ sripts/data/data-project.js
@@ -7,7 +7,7 @@ export const dataProject = [
         name: "Youtube project",
         img: "../images/project-thumb/Youtube.png",
         link: "../project/youtube-update/youtube.html",
-        description: "Ce projet est un clone de la mise en page  de la page d'accueil de Youtube afin de pratiquer les bases du HTML et du CSS. Le but ici était de reproduire l'interface de la page d'accueil de Youtube sans les interactions mais avec les positionnements des éléments,quelque des animations sur les élements et pratiquer un peu design responsiv.",
+        description: "Ce projet est une copie de la mise en page  de la page d'accueil de Youtube afin de pratiquer les bases du HTML et du CSS. Le but ici était de reproduire l'interface de la page d'accueil de Youtube sans les interactions mais avec les positionnements des éléments,quelque des animations sur les élements et pratiquer un peu design responsive.",
         techno: [
             htmlLogo,
             cssLogo
@@ -17,7 +17,7 @@ export const dataProject = [
         name: "Instagram project",
         img: "../images/project-thumb/Instagram.png",
         link: "../project/instagram/instagram.html",
-        description: "Ceci est un clone d'instagram qui est un exercice html css pour la mise en page",
+        description: "Ce projet est une copie d'un fil d'actualité Instagram également pour pratiquer le HTML et le CSS. Ici pour pousser plus sur le positionnement des éléments et pratiquer l'imbrication des élements les uns dans les autres. Ce projet est encore en cours de travail je souhaiterais ajouter un côter interactive avec du Javascript",
         techno: [
             htmlLogo,
             cssLogo
@@ -27,7 +27,7 @@ export const dataProject = [
         name: "Pierre feuille ciseaux",
         img: "../images/project-thumb/rock-paper-scissors.png",
         link: "../project/rock-paper-scissors/index.html",
-        description: "Ceci est un clone d'Amazon qui est un exercice html css pour la mise en page",
+        description: "Ce projet est un jeu de pierre feuille ciseaux contre un ordinateur, il est possible de faire tourner le jeux automatiquement. Le but de ce projet était de travailler sur les bases et certaine notions un peu plus avancéess du javascript tel que les varibles, les conditions, les fonctions, les boucles, la manipulation du DOM et le localStorage.",
         techno: [
             htmlLogo,
             cssLogo,
@@ -38,7 +38,7 @@ export const dataProject = [
         name: "Amazon project",
         img: "../images/project-thumb/Amazon.png",
         link: "../project/javascript-project/amazon.html",
-        description: "Ceci est un clone d'Amazon qui est un exercice html css pour la mise en page",
+        description: "Ce projet a pour but de recréer les fonctionnalité du site Amazon avec un catalogue de produit ajoutables dans un panier dont les quantités sont manipulable. Le but de ce projet est d'approfondir et affiner mes notions de javascript et les mettre en oeuvre sur un projet qui se rapproche un peu plus du monde proféssionel. Ce projet est un projet sur lequel je travail encore certaine fonctionnalité ne sont pas effectif pour le moment, seul l'ajout et la gestion du panier sont disponible.",
         techno: [
             htmlLogo,
             cssLogo,
