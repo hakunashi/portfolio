0bebccccb03b1bcaf486e46dc0c418ea8c079973
hakunashi <62364572+hakunashi@users.noreply.github.com>
2024-01-19 13:46:41 +0100

    Update data-project.js

--- sripts/data/data-project.js
+++ sripts/data/data-project.js
@@ -1,11 +1,11 @@
-const htmlLogo = "../images/techno/html5.png";
-const cssLogo = "../images/techno/css3.png";
-const javascriptLogo = "../images/techno/javascript.png";
+const htmlLogo = "./images/techno/html5.png";
+const cssLogo = "./images/techno/css3.png";
+const javascriptLogo = "./images/techno/javascript.png";
 
 export const dataProject = [
     {
         name: "Youtube project",
-        img: "../images/project-thumb/Youtube.png",
+        img: "./images/project-thumb/Youtube.png",
         link: "../project/youtube-update/youtube.html",
         description: "Ce projet est une copie de la mise en page  de la page d'accueil de Youtube afin de pratiquer les bases du HTML et du CSS. Le but ici était de reproduire l'interface de la page d'accueil de Youtube sans les interactions mais avec les positionnements des éléments,quelque des animations sur les élements et pratiquer un peu design responsive.",
         techno: [
@@ -15,7 +15,7 @@ export const dataProject = [
     },
     {
         name: "Instagram project",
-        img: "../images/project-thumb/Instagram.png",
+        img: "./images/project-thumb/Instagram.png",
         link: "../project/instagram/instagram.html",
         description: "Ce projet est une copie d'un fil d'actualité Instagram également pour pratiquer le HTML et le CSS. Ici pour pousser plus sur le positionnement des éléments et pratiquer l'imbrication des élements les uns dans les autres. Ce projet est encore en cours de travail je souhaiterais ajouter un côter interactive avec du Javascript",
         techno: [
@@ -25,7 +25,7 @@ export const dataProject = [
     },
     {
         name: "Pierre feuille ciseaux",
-        img: "../images/project-thumb/rock-paper-scissors.png",
+        img: "./images/project-thumb/rock-paper-scissors.png",
         link: "../project/rock-paper-scissors/index.html",
         description: "Ce projet est un jeu de pierre feuille ciseaux contre un ordinateur, il est possible de faire tourner le jeux automatiquement. Le but de ce projet était de travailler sur les bases et certaine notions un peu plus avancéess du javascript tel que les varibles, les conditions, les fonctions, les boucles, la manipulation du DOM et le localStorage.",
         techno: [
@@ -36,7 +36,7 @@ export const dataProject = [
     },
     {
         name: "Amazon project",
-        img: "../images/project-thumb/Amazon.png",
+        img: "./images/project-thumb/Amazon.png",
         link: "../project/javascript-project/amazon.html",
         description: "Ce projet a pour but de recréer les fonctionnalité du site Amazon avec un catalogue de produit ajoutables dans un panier dont les quantités sont manipulable. Le but de ce projet est d'approfondir et affiner mes notions de javascript et les mettre en oeuvre sur un projet qui se rapproche un peu plus du monde proféssionel. Ce projet est un projet sur lequel je travail encore certaine fonctionnalité ne sont pas effectif pour le moment, seul l'ajout et la gestion du panier sont disponible.",
         techno: [
