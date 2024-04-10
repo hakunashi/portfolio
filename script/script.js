42c8b9732983be6bd70573d62875b3da2756ba81
Isaac Rasolofo <isaacrasolofo@gmail.com>
2024-04-10 17:12:46 +0200

    update homepage

--- script/script.js
+++ script/script.js
@@ -1,15 +1,40 @@
 const burgerbtn = document.getElementById("burger-btn");
 const menuNav = document.getElementById("menu-nav");
+const containerImg = document.getElementById("container-items");
+const buttonSlides = document.querySelectorAll('#slide-btn button');
+
+let indexImagesActive = 0;
 
 const closeMenu = (event) => {
     console.log(event.target.closest('#burger-btn'))
     if (!event.target.closest('#burger-btn')) {
-        menuNav.classList.remove('active')
+        menuNav.classList.remove('active');
     }
 }
-
 document.addEventListener('click', closeMenu)
 
 burgerbtn.addEventListener('click',() => {
     menuNav.classList.toggle('active');
+});
+
+buttonSlides[0].addEventListener('click', () => {
+    indexImagesActive--;
+    if(indexImagesActive < 0) {
+        indexImagesActive = containerImg.children.length - 1;
+    }
+    console.log(indexImagesActive);
+    containerImg.style.transform = `translateX(-${indexImagesActive * 100}%)`;
 })
+
+buttonSlides[1].addEventListener('click', () => {
+    indexImagesActive++;
+    if(indexImagesActive >= containerImg.children.length) {
+        indexImagesActive = 0;
+    }
+    console.log(indexImagesActive);
+    containerImg.style.transform = `translateX(-${indexImagesActive * 100}%)`;
+})
+
+for(i=0,message="yo man"; i< 10; i++) {
+    console.log("toto",message)
+}
